50c039e0d6c4d8d2d09b2226c28ab2291f387378
wali-hu <abdullah.se4th@gmail.com>
2026-02-04 15:04:41 +0500

    Foundation of Fungibility (ERC-20)

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+};
